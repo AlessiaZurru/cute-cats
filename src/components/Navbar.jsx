9df44ac5f23ee80f488e3684969a07282ce55e81
Alessia Zurru <zurrualessia@gmail.com>
2022-04-09 15:24:44 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,18 @@ function Navbar() {
   return (
     <nav>
       <FontAwesomeIcon icon={faCat} className="nav--logo" />
-      <h1 className="nav--title">Adopt Cats</h1>
+      <h1 className="nav--title">Adopt a Cat</h1>
+      <div className="nav--items">
+        <div className="nav--items--contact">
+          <h4>Contact</h4>
+        </div>
+        <div className="nav--items--aboutus">
+          <h4>About us</h4>
+        </div>
+        <div className="nav--items--ourcats">
+          <h4>Our cats</h4>
+        </div>
+      </div>
     </nav>
   );
 }
